27436634f2be926abab53ee162a929689bcc8017
Matheus Ferreira <mths@edu.unifor.br>
2021-02-05 00:38:52 -0300

    server created, added first route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,20 @@
-console.log('a');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+// iniciando express na aplicação
+const app = express();
+
+// utilizando o cors
+app.use(cors());
+
+// definindo que a aplicação vai usar o padrão json
+app.use(express.json());
+
+// chamando as rotas
+app.use(routes);
+
+// metodo que vai chamar o servidor
+app.listen(3333, () => {
+  console.log('Server started on port 3333!! 🏆');
+});
