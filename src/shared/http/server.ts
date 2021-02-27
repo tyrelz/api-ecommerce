61e248ea1cdd29261d38654ddb10d15c14d36ab1
aa <aa>
2021-02-27 15:28:44 -0300

    database connection using docker

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppError';
+// import do metodo create connection
+import '@shared/typeorm';
 
 const app = express();
 
---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,5 @@
+import { createConnection } from 'typeorm';
+
+createConnection();
+
+// procura na estrutura de pastas do projeto pelo arquivo de config, dos parametros do banco de dados
